f88cad81e48beb8c1d442372908638dac50b8eac
Shishir Jindal <shishirjindal@users.noreply.github.com>
2016-02-29 04:08:53 +0530

    Update database_open.inc.php

--- database_open.inc.php
+++ database_open.inc.php
@@ -1,9 +1,9 @@
 <?php
 //first call my_sql connect function
-$host = 'mysql9.000webhost.com';
-$user = 'a5263108_1';
-$password = 'shishir123';
-if(!mysql_connect($host,$user,$password) || !mysql_select_db('a5263108_1'))
+$host = 'host_address';
+$user = 'your_username';
+$password = 'your_password';
+if(!mysql_connect($host,$user,$password) || !mysql_select_db('database_name'))
 {
 	echo mysql_error();
 }
